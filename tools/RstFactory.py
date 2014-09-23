91b65561720e179e910ba774d03ee29c885316d2
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2014-09-23 19:32:29 +0200

    update license header

--- tools/RstFactory.py
+++ tools/RstFactory.py
@@ -1,7 +1,7 @@
 ####################################################################################################
 # 
-# Babel - A Bibliography Manager.
-# Copyright (C) 2012 Salvaire Fabrice
+# Babel - A Bibliography Manager
+# Copyright (C) 2014 Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # 
 ####################################################################################################
 
