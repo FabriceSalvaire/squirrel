7d4b166bad7a9ce9e9bafd0568a050e9e3edd6e7
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-04-09 09:53:46 +0200

    setenv: mupdf binding

--- setenv.sh
+++ setenv.sh
@@ -5,6 +5,7 @@ append_to_path_if_not ${PWD}/tools
 
 append_to_ld_library_path_if_not /usr/local/stow/openjpeg2/lib/
 append_to_ld_library_path_if_not /usr/local/stow/mupdf-1.3/lib/
+export MUPDF_LIBRARY=/usr/local/stow/mupdf-1.3/lib/libmupdf.so
 
 source /srv/scratch/python-virtual-env/py3-pyqt5/bin/activate
 # source /srv/scratch/python-virtual-env/py3/bin/activate
