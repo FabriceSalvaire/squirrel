c801affdebfca4235c66641b96e592f4c79d2b09
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-11-21 00:11:39 +0100

    tools cleanup

--- setenv.sh
+++ setenv.sh
@@ -3,8 +3,9 @@ append_to_path_if_not ${PWD}/tools
 
 append_to_ld_library_path_if_not /usr/local/stow/mupdf/lib/
 export MUPDF_LIBRARY=/usr/local/stow/mupdf/lib/libmupdf-js-v8.so
-export LD_LIBRARY_PATH
+rm _mupdf.cpython-36m-x86_64-linux-gnu.so
+ln -sf build/lib.linux-x86_64-3.6/_mupdf.cpython-36m-x86_64-linux-gnu.so
 
-source /opt/python-virtual-env/py35/bin/activate
+source /opt/python-virtual-env/py36/bin/activate
 
 append_to_python_path_if_not ${PWD}
