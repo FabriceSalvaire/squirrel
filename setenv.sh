7b275306c3bb050e1535871ab5e4f6634e4898c8
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-05-04 01:28:04 +0200

    updated setenv

--- setenv.sh
+++ setenv.sh
@@ -1,15 +1,14 @@
-append_to_path_if_not ${PWD}/bin
-append_to_path_if_not ${PWD}/tools
+# append_to_path_if_not ${PWD}/bin
+# append_to_path_if_not ${PWD}/tools
 
-# append_to_ld_library_path_if_not /usr/local/stow/mupdf/lib/
-# export MUPDF_LIBRARY=/usr/local/stow/mupdf/lib/libmupdf-js-v8.so
+# append_to_ld_library_path_if_not /usr/local/stow/mupdf-1.12/lib/
+# export MUPDF_LIBRARY=/usr/local/stow/mupdf-1.12/lib/libmupdf.so
+export MUPDF_LIBRARY_PATH=/usr/local/stow/mupdf-1.12/lib
 
-append_to_ld_library_path_if_not /usr/local/stow/mupdf-1.12/lib/
-export MUPDF_LIBRARY=/usr/local/stow/mupdf-1.12/lib/libmupdf.so
-
-rm _mupdf.cpython-36m-x86_64-linux-gnu.so
+/bin/rm -f _mupdf.cpython-36m-x86_64-linux-gnu.so
 ln -sf build/lib.linux-x86_64-3.6/_mupdf.cpython-36m-x86_64-linux-gnu.so
 
 source /opt/python-virtual-env/py36/bin/activate
 
 append_to_python_path_if_not ${PWD}
+append_to_python_path_if_not ${PWD}/external-packages/
