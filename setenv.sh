cea2c890411af8ea41c5fef90eafdd23d6be4e12
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2019-02-28 21:02:20 +0100

    py37

--- setenv.sh
+++ setenv.sh
@@ -8,7 +8,7 @@ export MUPDF_LIBRARY_PATH=/usr/local/stow/mupdf-1.12/lib
 /bin/rm -f _mupdf.cpython-36m-x86_64-linux-gnu.so
 ln -sf build/lib.linux-x86_64-3.6/_mupdf.cpython-36m-x86_64-linux-gnu.so
 
-source /opt/python-virtual-env/py36/bin/activate
+source /opt/python-virtual-env/py37/bin/activate
 
 append_to_python_path_if_not ${PWD}
 append_to_python_path_if_not ${PWD}/external-packages/
