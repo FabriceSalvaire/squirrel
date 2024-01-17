8bcbdf614abeded3b87a34d5ef910cb65ec27e0c
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-01-17 15:29:41 +0100

    setenv

--- setenv.sh
+++ setenv.sh
@@ -5,10 +5,12 @@
 # export MUPDF_LIBRARY=/usr/local/stow/mupdf-1.12/lib/libmupdf.so
 export MUPDF_LIBRARY_PATH=/usr/local/stow/mupdf-1.12/lib
 
+_MUPDF_SO=${_MUPDF_SO}
 /bin/rm -f _mupdf.cpython-36m-x86_64-linux-gnu.so
-ln -sf build/lib.linux-x86_64-3.6/_mupdf.cpython-36m-x86_64-linux-gnu.so
+ln -sf build/lib.linux-x86_64-cpython-312/${_MUPDF_SO}
 
-source /opt/python-virtual-env/py37/bin/activate
+# source /opt/python-virtual-env/py312/bin/activate
+py312
 
 append_to_python_path_if_not ${PWD}
 append_to_python_path_if_not ${PWD}/external-packages/
